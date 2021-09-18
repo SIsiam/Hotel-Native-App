565f8bf71e9d15daa8ba7bbb679678e098eca955
sisiam <shahidulmd392@gmail.com>
2021-09-18 13:52:27 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "react-native-gesture-handler";
+import { StatusBar } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./src/views/HomeScreen";
+import COLORS from "./src/consts/colors";
+import DetailsScreen from "./src/views/DetailsScreen";
+const Stack = createStackNavigator();
+const App = () => {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar backgroundColor={COLORS.white} barStyle="dark-content" />
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+     );
+    };
+    
+    export default App;
---
+++ README.md
@@ -0,0 +1,14 @@
+## Hostel Circle -Hostel Booking App
+
+#### Developer:  Shahidul Islam Siam
+#### Developer : Mijanul Karim
+#### Developer : MD SHAHINUR KHAN
+
+
+This is Hostel Booking React Native App where user can search and book the hostel room.
+
+### React Native, React Navigation
+
+
+
+
